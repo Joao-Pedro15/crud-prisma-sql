2e0d5861ad15caab620dd2ebfb34cf34de492ed0
João Pedro <jjoao.monteiro15@gmail.com>
2022-08-22 22:10:08 -0300

    test: create a test from function create post in route post

--- test/post.test.js
+++ test/post.test.js
@@ -16,6 +16,22 @@ describe('Tests from post controllers', () => {
         notDeepStrictEqual(key, null)
       }
     })
+
+    describe('/post', () => {
+      it('should request to create a post and return HTTP status 200', async () => {
+        const body = {
+          content: 'Conteúdo do post'
+        }
+        const response = await request(app)
+        .post('/post/user/4')
+        .send(body)
+        .expect(200)
+        deepStrictEqual(response.status, 200)
+        for await (let key of Object.values(response.body)){
+          notDeepStrictEqual(key, null)
+        }
+      })
+    })
   })
 
 })
