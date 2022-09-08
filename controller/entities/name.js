50ddf610e68bbbe766f60ce613decce804ebfc3d
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-08 20:31:05 -0300

    chore: change name function validate in class Name

--- controller/entities/name.js
+++ controller/entities/name.js
@@ -3,7 +3,12 @@ export class Name {
         this.value = name
     }
 
-    valid (name) {
+    create(name){
+        if(!this.validate(name)) throw new Error('Invalid name')
+        return new Name(name)
+    }
+
+    validate (name) {
         if(!name) return false
         if(name.trim().length < 2 || name.trim().length > 256) return false
         return true
