9eee3f359e51a8db88e5c5fd7212f46065363fa5
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-08 20:30:41 -0300

    feat: create a class validate email per using in create user

---
+++ controller/entities/email.js
@@ -0,0 +1,17 @@
+export class Email {
+    constructor(email) {
+        this.value = email
+    }
+
+    create (email) {
+        if(!this.validate(email)) throw new Error('Invalid email!')
+        return new Email(email)
+    }
+
+    validate (email) {
+        if(!email) return false
+        if(email.length > 320) return false
+        if(!email.includes('@')) return false
+        return true
+    }
+}
