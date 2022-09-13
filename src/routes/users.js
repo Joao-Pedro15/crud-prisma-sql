75cc603a3751c8c108e3557b21b1fc666b2d774b
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-13 19:50:08 -0300

    feat: passing a method handle from CreateUserController class

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,10 @@
 const { Router } = require('express')
+const CreateUserController = require('../controller/user/createUser')
+
 const router = Router()
+
+
+router.post('/createUser', CreateUserController.handle)
 
 
 module.exports = router
