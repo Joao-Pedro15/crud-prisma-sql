319abad6324163ef00efc7535daf76d42d6669c7
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-12 22:21:23 -0300

    feat: create useCase from createPost

---
+++ src/controller/post/createPost/CreatePostUseCase.js
@@ -0,0 +1,11 @@
+class CreatePostUseCase {
+    async execute ({ author, content }) {
+        try {
+            return
+        } catch (error) {
+            return error
+        }
+    }
+}
+
+module.exports = new CreatePostUseCase()
