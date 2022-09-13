8438976c50e6e2724eb05abb0fe4b77605181366
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-13 19:46:26 -0300

    feat: instance class create user and using in useCase user

--- src/controller/user/createUser/CreateUserController.js
+++ src/controller/user/createUser/CreateUserController.js
@@ -1,13 +1,17 @@
 const CreateUserUseCase = require("./CreateUserUseCase")
+const User = require('../../../entities/user/user')
 
 class CreateUserController {
     async handle(request, response) {
         const { email, password } = request.body
+        const UserData = User.create(email, password)
         try {
-            const user = CreateUserUseCase.execute({ email,  })
+            const user = CreateUserUseCase.execute(UserData)
             return response.status(201).json(user)
         } catch (error) {
             return response.status(error.statusCode).json({message: error.message})
         }
     }
 }
+
+module.exports = new CreateUserController()
