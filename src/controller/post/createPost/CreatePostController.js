0500978b2d4645a70cb40eecefbc075785d9a6a7
João Pedro <jjoao.monteiro15@gmail.com>
2022-09-15 22:06:28 -0300

    chore: passing params from entitie create post

--- src/controller/post/createPost/CreatePostController.js
+++ src/controller/post/createPost/CreatePostController.js
@@ -1,10 +1,12 @@
 const CreatePostUseCase = require("./CreatePostUseCase")
+const Post = require('../../../entities/post/content')
 
 class CreatePostController {
     async handle (request, response) {
         const { author, content } = request.body
+        const postData = Post.create(author, content)
         try {
-            const contentFinal = await CreatePostUseCase.execute({ author, content })
+            const contentFinal = await CreatePostUseCase.execute(postData)
             return response.status(200).json(contentFinal)
         } catch (error) {
             return response.status(200).json({message: error.message})
